fd37ba9cd41176dda50a68ded53c72dbaa46a965
struk <halynkastru@gmail.com>
2022-07-30 16:30:53 +0300

    axios + Form(useRef)

--- src/components/Form2/Form2.js
+++ src/components/Form2/Form2.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Form2 = () => {
+const Form2 = () => { // контрольовані форми
+
+    const [user, setUser] = useState({})
+
+    const getUser = (e) => {
+        // console.log(e.target.name);
+        // console.log(e.target.value);
+        setUser({...user,[e.target.name]:e.target.value})
+
+    }
     return (
         <div>
-            <div><label>Name<input type="text"/></label></div>
-            <div><label>Age<input type="number"/></label></div>
-            <button>save</button>
+            <div><label>Name<input type="text" name={'name'} onChange={getUser}/></label></div>
+            <div><label>Age<input type="number" name={'age'} onChange={getUser}/></label></div>
+            {JSON.stringify(user)}
         </div>
     );
 };
