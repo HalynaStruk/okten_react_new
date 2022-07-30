53689a5ae7ff7d551b130e2b568db091fdfefcf8
struk <halynkastru@gmail.com>
2022-07-31 00:18:14 +0300

    homework axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import {Posts, UserDetails, Users} from "./components";
+import css from './App.module.css'
 
 const App = () => {
     return (
         <div>
-            <div>
+            <div className={css.wrap}>
             <Users/>
             <UserDetails/>
             </div>
